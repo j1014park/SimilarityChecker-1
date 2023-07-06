d9fc65e890964488c75fe5e0c1e7a428d03704cb
j1014park <hero20061014@gmail.com>
2023-07-06 11:27:16 +0900

    1단계 ThrowExceptionInvalidCase1 Commit

--- SimilarityChecker-1/SimilarityChecker.cpp
+++ SimilarityChecker-1/SimilarityChecker.cpp
@@ -1,5 +1,27 @@
+#include <string>
+#include <stdexcept>
+using namespace std;
 class SimilarityChecker
 {
 public:
-	
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		for (char ch : guessNumber)
+		{
+			if (ch >= 'A' && ch <= 'Z')
+				continue;
+			throw invalid_argument("Must be number");
+		}
+	}
+
+	int getPoint(const string& firstString, const string& secondString)
+	{
+		int point = 0;
+		assertIllegalArgument(firstString);
+		assertIllegalArgument(secondString);
+		return point;
+	}
+
+private:
+
 };
--- SimilarityCkecker-Test/SimilarityChecker_UnitTest.cpp
+++ SimilarityCkecker-Test/SimilarityChecker_UnitTest.cpp
@@ -1,6 +1,29 @@
 #include "pch.h"
+#include "../SimilarityChecker-1/SimilarityChecker.cpp"
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
+	SimilarityChecker similarityChecker;
+	void assertIllegalArgument(string firstString, string secondString)
+	{
+		try
+		{
+			similarityChecker.getPoint(firstString,secondString);
+			FAIL();
+		}
+		catch (exception e)
+		{
+		}
+	}
+};
+
+TEST_F(SimilarityCheckerFixture, ThrowExceptionInvalidCase1) {
+	assertIllegalArgument("1ABC","CCD");
 }
+
+TEST_F(SimilarityCheckerFixture, ThrowExceptionInvalidCase2) {
+	assertIllegalArgument("CCD","C1CD");
+}
+
